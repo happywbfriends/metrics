39583ea845bcc025606b1c21e10ac3c0481326f9
Mike Wozowoy <mike@stch.ru>
2023-08-14 13:21:51 +0300

    косметика

--- v1/http_srv.go
+++ v1/http_srv.go
@@ -8,30 +8,30 @@ import (
 	"github.com/happywbfriends/metrics/metrics"
 )
 
-type HTTPServerRequestMetricsV1 interface {
+func NewHTTPServerRequestMetrics() HTTPServerRequestMetrics {
+	return NewHttpServerRequestMetricsWithBuckets(metrics.DefaultDurationMsBuckets)
+}
+
+type HTTPServerRequestMetrics interface {
 	IncNbRequest(method string, statusCode int, supplierOldId int)
 	ObserveRequestDuration(method string, duration time.Duration)
 }
 
-type httpServerRequestMetricsV1 struct {
+type httpServerRequestMetrics struct {
 	nbRequests    *prometheus.CounterVec
 	requestTimeMs *prometheus.HistogramVec
 }
 
-func (m *httpServerRequestMetricsV1) IncNbRequest(method string, statusCode int, supplierOldId int) {
+func (m *httpServerRequestMetrics) IncNbRequest(method string, statusCode int, supplierOldId int) {
 	m.nbRequests.WithLabelValues(method, strconv.Itoa(statusCode), strconv.Itoa(supplierOldId)).Inc()
 }
 
-func (m *httpServerRequestMetricsV1) ObserveRequestDuration(method string, duration time.Duration) {
+func (m *httpServerRequestMetrics) ObserveRequestDuration(method string, duration time.Duration) {
 	m.requestTimeMs.WithLabelValues(method).Observe(float64(duration.Milliseconds()))
 }
 
-func NewHTTPServerRequestMetricsV1() HTTPServerRequestMetricsV1 {
-	return NewHttpServerRequestMetricsWithBucketsV1(metrics.DefaultDurationMsBuckets)
-}
-
-func NewHttpServerRequestMetricsWithBucketsV1(requestTimeMsBuckets []float64) HTTPServerRequestMetricsV1 {
-	m := &httpServerRequestMetricsV1{
+func NewHttpServerRequestMetricsWithBuckets(requestTimeMsBuckets []float64) HTTPServerRequestMetrics {
+	m := &httpServerRequestMetrics{
 		nbRequests:    metrics.NewCounterVec(metrics.MetricsNamespace, metrics.MetricsSubsystemHttpServer, "nb_req", nil, []string{metrics.MetricsLabelMethod, metrics.MetricsLabelStatusCode, metrics.MetricsLabelSupplierOldId}),
 		requestTimeMs: metrics.NewHistogramVec(metrics.MetricsNamespace, metrics.MetricsSubsystemHttpServer, "req_duration_ms", nil, requestTimeMsBuckets, []string{metrics.MetricsLabelMethod}),
 	}
