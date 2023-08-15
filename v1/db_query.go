feeaee1c67b1eaf7cb20c4b618e9bef5c198fdd8
Mike Wozowoy <mike@stch.ru>
2023-08-15 11:51:20 +0300

    Добавлена версия v1 для метрик db_query

--- v1/db_query.go
+++ v1/db_query.go
@@ -6,64 +6,32 @@ import (
 	"time"
 )
 
-type DbQueryMetrics interface {
-	IncNbDone()
-	IncNbError(error)
-	ObserveRequestDuration(time.Duration)
+func NewDbQueryMetrics() DbQueryMetrics {
+	return &dbRequestMetrics{
+		durationMs: metrics.NewHistogramVec(metrics.MetricsNamespace, metrics.MetricsSubsystemDbQuery, "duration_ms", nil, metrics.DefaultDurationMsBuckets, []string{MetricsLabelSubject, metrics.MetricsLabelDatabaseQuery}),
+		nbDone:     metrics.NewCounterVec(metrics.MetricsNamespace, metrics.MetricsSubsystemDbQuery, "nb_done", nil, []string{MetricsLabelSubject, metrics.MetricsLabelDatabaseQuery}),
+		nbError:    metrics.NewCounterVec(metrics.MetricsNamespace, metrics.MetricsSubsystemDbQuery, "nb_error", nil, []string{MetricsLabelSubject, metrics.MetricsLabelDatabaseQuery}),
+	}
 }
 
-func NewDbQueryMetrics(dbName, queryName string) DbQueryMetrics {
-	labels := map[string]string{
-		MetricsLabelSubject:               dbName,
-		metrics.MetricsLabelDatabaseQuery: queryName,
-	}
-
-	return &dbRequestMetrics{
-		durationMs: metrics.NewHistogram(metrics.MetricsNamespace, metrics.MetricsSubsystemDbQuery, "duration_ms", labels, metrics.DefaultDurationMsBuckets),
-		nbDone:     metrics.NewCounter(metrics.MetricsNamespace, metrics.MetricsSubsystemDbQuery, "nb_done", labels),
-		nbError:    metrics.NewCounter(metrics.MetricsNamespace, metrics.MetricsSubsystemDbQuery, "nb_error", labels),
-	}
+type DbQueryMetrics interface {
+	IncNbDone(subject string, query string)
+	IncNbError(subject string, query string)
+	ObserveRequestDuration(subject string, query string, duration time.Duration)
 }
 
 type dbRequestMetrics struct {
-	durationMs prometheus.Histogram
-	nbDone     prometheus.Counter
-	nbError    prometheus.Counter
+	durationMs *prometheus.HistogramVec
+	nbDone     *prometheus.CounterVec
+	nbError    *prometheus.CounterVec
 }
 
-func (m *dbRequestMetrics) ObserveRequestDuration(duration time.Duration) {
-	m.durationMs.Observe(float64(duration.Milliseconds()))
+func (m *dbRequestMetrics) IncNbDone(subject string, query string) {
+	m.nbDone.WithLabelValues(subject, query).Inc()
 }
-func (m *dbRequestMetrics) IncNbDone() {
-	m.nbDone.Inc()
+func (m *dbRequestMetrics) IncNbError(subject string, query string) {
+	m.nbError.WithLabelValues(subject, query).Inc()
 }
-func (m *dbRequestMetrics) IncNbError(error) {
-	m.nbError.Inc()
-}
-
-/*
-Helper для быстрого расчета метрик запроса. Плюсом идет то, что метод сам анализирует ошибку и может инкрементить
-нужные вспомогательные метрики.
-
-Пример:
-
-```
-
-	func SomeDatabaseMethod() (e error){
-		defer func(from time.Time) {
-			metrics.DbQueryMetricsHelper(metrics, from, e)
-		}(time.Now())
-
-		// Your code goes here
-	}
-
-```
-*/
-func DbQueryMetricsHelper(m DbQueryMetrics, startTm time.Time, err error) {
-	m.ObserveRequestDuration(time.Since(startTm))
-	if err != nil {
-		m.IncNbError(err)
-	} else {
-		m.IncNbDone()
-	}
+func (m *dbRequestMetrics) ObserveRequestDuration(subject string, query string, duration time.Duration) {
+	m.durationMs.WithLabelValues(subject, query).Observe(float64(duration.Milliseconds()))
 }
